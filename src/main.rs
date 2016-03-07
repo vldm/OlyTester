0114f153e860850d42c26856921caefe8e621997
Vladimir Motilenko <c-vld@ya.ru>
2016-03-07 23:10:40 +0200

    Add init process module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate time;
+extern crate libc;
+
+mod process;
+use process::{ProcessConfig, Verdict};
+
 fn main() {
-    println!("Hello, world!");
+		let input = vec!["test".to_owned(),"test2".to_owned()];
+		let output = input.clone();
+		let x = ProcessConfig::new("tac").run((input,output));
+	    assert_eq!(x,Verdict::WrongOutput);
+	    println!("{:?}", x);
 }
